4db123e2b603ee604913ee76e712cd95e1c99565
SafeLightToken <82354839+SafeLightToken@users.noreply.github.com>
2021-04-18 17:03:58 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	// Grab the current date
 	var currentDate = new Date();
 	// Set some date in the future. In this case, it's always Jan 1
-	var futureDate  = new Date(2021,03,21,00,00,00);
+	var futureDate  = new Date(2021,03,25,00,00,00);
 	// Calculate the difference in seconds between the future and current date
 	var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 	// Instantiate a coutdown FlipClock
